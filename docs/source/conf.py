2e1b9799f8d250dcf34f918077cd4599fa8bcc3b
Jubliani <59886167+Jubliani@users.noreply.github.com>
2022-06-15 20:46:04 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Unity Documentation'
+copyright = '2022, Jubliani'
+author = 'Jubliani'
 
 release = '0.1'
 version = '0.1.0'
